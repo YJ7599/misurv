9699a4642162e9058f50051915de0d942ff18521
YJ7599 <109124970+YJ7599@users.noreply.github.com>
2023-03-02 13:55:39 +0900

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -394,6 +394,9 @@ library(splitTools)
         
         ##### Model 2: Taxa Analysis ####
         tabItem(tabName = "thelasttrail", br(),
+                
+                mainPanel(width = 9, uiOutput("baram")),
+                
                 sidebarLayout( 
                   position = "left",
                   div(style="width: 98%;",sidebarPanel(width = 3,
@@ -402,7 +405,7 @@ library(splitTools)
                                                        uiOutput("covariates_taxa"), 
                                                        actionButton("button_bal", (strong("Run!")), class = "btn-info"),
                                                        uiOutput("dttttfragile"),
-                                                       uiOutput("rtttt.anti"))),
+                                                       uiOutput("rtttt.anti")))
                   
                   mainPanel(width = 9, uiOutput("baram")) 
                   
