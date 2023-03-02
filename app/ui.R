26ca9059b7ee550b17e7cd8410df4033d2ba6242
YJ7599 <109124970+YJ7599@users.noreply.github.com>
2023-03-02 11:17:48 +0900

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -422,7 +422,7 @@ library(splitTools)
                   
                   mainPanel(width = 9,
                             fluidRow(width = 8, 
-                                     div(style='height:800px;overflow-y: scroll;', uiOutput("obj__g")), br(),br(),
+                                     div(style='height:800px;overflow-y: scroll;', uiOutput("kkkirings")), br(),br(),
                                      uiOutput("nine_five_four"))
                   ))),
         
