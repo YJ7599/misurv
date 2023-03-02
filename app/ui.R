5c3a90ba4abd93ad4862d3ae3af98c985aad7309
YJ7599 <109124970+YJ7599@users.noreply.github.com>
2023-03-02 10:58:51 +0900

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -422,7 +422,7 @@ library(splitTools)
                   
                   mainPanel(width = 9,
                             fluidRow(width = 8, 
-                                     div(style='height:800px;overflow-y: scroll;', uiOutput("airpods")), br(),br(),
+                                     div(style='height:800px;overflow-y: scroll;', uiOutput("obj/g")), br(),br(),
                                      uiOutput("nine_five_four"))
                   ))),
         
